0ff9d157af1052de6c1ae1a513bb92b7b2b39395
Antoine Marionneau <58473024+AntoineMra@users.noreply.github.com>
2022-12-19 15:48:53 +0100

    Template Search Service

--- src/Service/Search/GoogleSearchService.php
+++ src/Service/Search/GoogleSearchService.php
@@ -4,16 +4,32 @@ namespace App\Service;
 
 class GoogleSearchService
 {
-    public function getHappyMessage(): string
+    public function executeSearchService(string $body): string
     {
-        $messages = [
-            'You did it! You updated the system! Amazing!',
-            'That was one of the coolest updates I\'ve seen all day!',
-            'Great work! Keep going!',
+        $options = [
+            'body' => $body,
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+                // Add API KEY + Query Params ?
+            ],
         ];
+        // Add %env(url)
+        $response = $this->brmsClient->request('POST', self::PATH_EXECUTE_DECISION_SERVICE, $options);
 
-        $index = array_rand($messages);
+        if ($response->getStatusCode() !== Response::HTTP_OK) {
+            // Throw Log Error in Logger
+        }
 
-        return $messages[$index];
+        if (!str_contains($response->getHeaders()['content-type'][0], 'application/json')) {
+             // Throw Log Error in Logger
+        }
+
+        $content = $response->getContent();
+        if ($content === '') {
+             // Throw Log Error in Logger
+        }
+
+        return $content;
     }
 }
