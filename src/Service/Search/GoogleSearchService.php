984f60498f5906c336560ed22245ac95f57cb793
Antoine Marionneau <58473024+AntoineMra@users.noreply.github.com>
2022-12-20 11:50:40 +0100

    Update GoogleSearchService.php

--- src/Service/Search/GoogleSearchService.php
+++ src/Service/Search/GoogleSearchService.php
@@ -2,32 +2,41 @@
 
 namespace App\Service;
 
+use Psr\Log\LoggerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 class GoogleSearchService
 {
+    public function __construct(
+        private readonly HttpClientInterface $client,
+        private readonly LoggerInterface $logger
+    ) {
+    }
+    
     public function executeSearchService(string $body): string
     {
         $options = [
-            'body' => $body,
+            'body' => $body, // Check if body is necessary in google documentatin
             'headers' => [
                 'Content-Type' => 'application/json',
                 'Accept' => 'application/json',
                 // Add API KEY + Query Params ?
             ],
         ];
-        // Add %env(url)
-        $response = $this->brmsClient->request('POST', self::PATH_EXECUTE_DECISION_SERVICE, $options);
+        // Add %env(url) get env ? sinon parametrer un argument dans serivce.yml
+        $response = $this->client->request('POST', self::PATH_EXECUTE_DECISION_SERVICE, $options);
 
         if ($response->getStatusCode() !== Response::HTTP_OK) {
-            // Throw Log Error in Logger
+            $this->logger->error($response->getStatusCode());
         }
 
         if (!str_contains($response->getHeaders()['content-type'][0], 'application/json')) {
-             // Throw Log Error in Logger
+             $this->logger->error('Wrong content type');
         }
 
         $content = $response->getContent();
         if ($content === '') {
-             // Throw Log Error in Logger
+             $this->logger->error('Content is empty');
         }
 
         return $content;
